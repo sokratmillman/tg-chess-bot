33a927b5ec748d41a9884866f4fea32ff9c028d7
dapp-anyway <69850903+dapp-anyway@users.noreply.github.com>
2023-05-10 04:06:41 +0300

    feat: ai game implementation

---
+++ handlers/ai_game_handlers.py
@@ -0,0 +1,104 @@
+from aiogram import  Dispatcher, types
+from aiogram.dispatcher import FSMContext
+import chess
+import chess.svg
+import os
+from const import AVAILABLE
+from handlers.state import ChessState
+from stockfish import Stockfish
+
+from libs.draw_board import draw_board
+from bot import bot, dp
+
+from firebase.db_handlers import database, handle_game_start, handle_game_finish
+from texts import AI_WON_TEXT, COMPUTER_MOVE, DRAW_TEXT, INVALID_MOVE_TEXT, NOT_YOUR_TURN_TEXT, GAME_STARTED_TEXT, WIN_TEXT, WRONG_START_TEXT
+
+
+engine_path = os.getcwd()+"/stockfish_15.1_linux_x64/stockfish-ubuntu-20.04-x86-64"
+
+stockfish = Stockfish(path=engine_path)
+stockfish.depth = 1
+
+async def start_ai_game(message: types.Message, state: FSMContext):
+    board = chess.Board()
+
+    user = database.get_user(message.from_user.id)
+
+    if user is None:
+        await message.answer(WRONG_START_TEXT)
+        await state.finish()
+        return
+
+    await draw_board(message, board)
+    
+    await message.answer(GAME_STARTED_TEXT)
+    await state.set_state(ChessState.AI_GAME.state)
+
+    handle_game_start(str(message.from_user.id), 0)
+
+async def ai_make_turn(message: types.Message, state: FSMContext):
+    user = database.get_user(message.from_user.id)
+    if user is None:
+        await state.finish()
+        return
+    game_id = user['current_game']
+    if (game_id == '-1'):
+        # no game in progress, ignore
+        await state.finish()
+        return
+    game = database.get_game(game_id)
+    white_id = game['white']
+    black_id = game['black']
+
+    if (game['current_turn'] != str(message.from_user.id)):
+        await message.answer(NOT_YOUR_TURN_TEXT)
+        return
+
+    partner_id = 0
+
+    board = chess.Board(game['board'])
+    board.turn = chess.WHITE
+
+    try:
+        board.push_san(message.text)
+        await draw_board(message, board)
+        database.update_game(game_id, {"current_turn": str(partner_id), "board": board.fen()})
+
+        if board.is_game_over():
+            if board.is_checkmate():
+                handle_game_finish(game_id, 'win', 'ai', 'white')
+                await message.answer(WIN_TEXT.format('You'))
+            else:
+                handle_game_finish(game_id, 'draw', 'ai', 'white')
+                await message.answer(DRAW_TEXT)
+            await state.finish()
+
+        else:
+                partner_id = message.from_user.id
+
+                board = chess.Board(board.fen())
+                board.turn = chess.BLACK
+                
+                stockfish.set_fen_position(board.fen())
+
+                best_move = stockfish.get_best_move()
+                board.push_san(best_move)
+
+                await message.answer(COMPUTER_MOVE.format(best_move))
+                await draw_board(message, board)
+
+                if board.is_game_over():
+                    if board.is_checkmate():
+                        handle_game_finish(game_id, 'loss', 'ai')
+                        await message.answer(AI_WON_TEXT)
+                    else:
+                        await message.answer(DRAW_TEXT)
+                    await state.finish()
+
+                database.update_game(game_id, {"current_turn": str(partner_id), "board": board.fen()})
+    except Exception:
+        await message.answer(INVALID_MOVE_TEXT)
+
+def register_ai_game_handlers(dp: Dispatcher):
+    dp.register_message_handler(start_ai_game, commands="start_ai_game")
+    dp.register_message_handler(ai_make_turn, state=ChessState.AI_GAME)
---
+++ handlers/common_handlers.py
@@ -0,0 +1,104 @@
+from aiogram import Dispatcher, types
+from aiogram.dispatcher import FSMContext
+from const import AVAILABLE
+
+from firebase.db_handlers import database, handle_game_finish
+from bot import bot, dp
+from texts import GAME_CANCELLED_TEXT, HELP_TEXT, NO_GAME_YET_TEXT, YOUR_ID_TEXT
+
+async def send_welcome(message: types.Message):
+    await message.answer("Hi!\nI'm ChessBot!\nPowered by aiogram.")
+
+    database.create_user(message.from_user.id)
+
+async def get_info(message: types.Message):
+    await message.answer(HELP_TEXT, parse_mode='Markdown')
+
+async def get_my_id(message: types.Message):
+    
+    await message.answer(YOUR_ID_TEXT.format(message.from_user.id), parse_mode='Markdown')
+
+async def cancel_game(message: types.Message, state: FSMContext):
+    user = database.get_user(message.from_user.id)
+    if user is None:
+        await state.finish()
+        await message.answer(GAME_CANCELLED_TEXT)
+        return
+    
+    game_id = user['current_game']
+    if (game_id == '-1'):
+        await message.answer(NO_GAME_YET_TEXT)
+        await state.finish()
+        return
+    
+    game = database.get_game(game_id)
+    white_id = game['white']
+    black_id = game['black']
+    if (black_id == '0'):
+        await message.answer(GAME_CANCELLED_TEXT)
+        await state.finish()
+        user = database.get_user(message.from_user.id)
+        game_id = user['current_game']
+        handle_game_finish(game_id, 'canceled', 'ai')
+        return
+    
+    partner_id = white_id if str(message.from_user.id) != white_id else black_id
+    partner_state = dp.current_state(user=partner_id, chat=partner_id)
+    await partner_state.finish()
+    await state.finish()
+    
+    await message.answer(GAME_CANCELLED_TEXT)
+    await bot.send_message(partner_id, GAME_CANCELLED_TEXT)
+    handle_game_finish(game_id, 'canceled', 'pvp')
+
+async def leaderboard(message: types.Message):
+    leaders = database.get_leaderboard()
+
+    if leaders is None:
+        await message.answer("No leaders yet")
+        return
+
+    result = f"Top-{len(leaders)} players:"
+    for key in leaders:
+        result += f"{key}. {leaders[key]}\n"
+    await message.answer(result)
+
+async def my_history(message: types.Message):
+    history = database.get_user_history(message.from_user.id)
+
+    if history == '':
+        await message.answer("No recent games")
+        return
+
+    history_list = history.split(", ")
+    result = f"Most current games:\n\n"
+    for el in history_list:
+        state, id = el.split(" ")[0], el.split(" ")[1]
+        if state == "won":
+            result += f"You won {id}\n"
+        elif state == "loss":
+            result += f"You lost to {id}\n"
+        else:
+            result += f"Draw with {id}\n"
+    await message.answer(result)
+
+async def my_stats(message: types.Message):
+    stats = database.get_user_stats(message.from_user.id)
+
+    result = f"Your statistics:\n\n"
+    result += f"Total games: {stats['total games']}\n"
+    result += f"Wins: {stats['wins']}\n"
+    result += f"Losses: {stats['losses']}\n"
+    result += f"Draws: {stats['draws']}\n"
+    result += f"% of wins: {stats['% of wins']}"
+    
+    await message.answer(result)
+
+def register_common_handlers(dp: Dispatcher):
+    dp.register_message_handler(send_welcome, commands="start")
+    dp.register_message_handler(get_info, commands="help", state="*")
+    dp.register_message_handler(get_my_id, commands="get_my_id", state="*")
+    dp.register_message_handler(cancel_game, commands="cancel_game", state="*")
+    dp.register_message_handler(my_stats, commands="my_stats", state="*")
+    dp.register_message_handler(my_history, commands="my_history", state="*")
+    dp.register_message_handler(leaderboard, commands="leaderboard", state="*")
