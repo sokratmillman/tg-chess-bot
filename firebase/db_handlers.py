ba68b885c2a16f3aa36789252f277780f5988ad1
sokratmillman <superstepashka1981@gmail.com>
2023-05-15 10:44:55 +0300

    fix: remove games after finish

--- firebase/db_handlers.py
+++ firebase/db_handlers.py
@@ -149,6 +149,7 @@ def handle_pvp_finish(game_id, status, winner_color=None):
         loser_id = white_uid if winner_color=="black" else black_uid
         handle_win_for_user(winner_id, loser_id)
         handle_loss_for_user(loser_id, winner_id)
+        database.delete_game(game_id)
 
     database.update_user(black_uid, {"current_game": "-1", "status": "AVAILABLE"})
     database.update_user(white_uid, {"current_game": "-1", "status": "AVAILABLE"})
@@ -169,6 +170,7 @@ def handle_ai_finish(game_id, status, winner_color=None):
             handle_loss_for_user(player_uid)
 
     database.update_user(player_uid, {"current_game": "-1", "status": "AVAILABLE"})
+    database.delete_game(game_id)
 
 def handle_game_finish(game_id, status, game_type, winner_color=None):
     if game_type=="ai":
